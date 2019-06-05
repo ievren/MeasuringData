d10db6f94378fd0454b1bbce2a3fbe4b89d74558
ievren <kasperlifan@gmail.com>
2019-06-05 17:50:38 +0200

    Revert "added copyright header"
    This reverts commit c47715ed

--- app/src/main/java/ch/zhaw/android/measuringdata/engine/Engine.java
+++ app/src/main/java/ch/zhaw/android/measuringdata/engine/Engine.java
@@ -1,21 +1,3 @@
-/*
- * Zuercher Hochschule fuer Angewandte Wissenschaften (ZHAW)
- * School of Engineering (SoE)
- * InES Institut of Embedded Systems
- *
- *   Bachelorarbeit BA19_gruj_10
- *   Projekt FMS
- *   Darius Eckhardt (eckhadar)
- *   Ibrahim Evren   (evrenibr)
- *
- *   07.06.2019, Winterthur Switzerland
- *
- *   This Software is based on the ble_app_uart, SDK Version 15.2.0
- *   It has been modified to fit the needs of the Project FMS
- *   For correct functionality this Software has to be placed into the same folder as the SDK.
- *
- */
-
 package ch.zhaw.android.measuringdata.engine;
 
 import android.content.Context;
--- app/src/main/java/ch/zhaw/android/measuringdata/utils/Utils.java
+++ app/src/main/java/ch/zhaw/android/measuringdata/utils/Utils.java
@@ -1,19 +1,23 @@
 /*
- * Zuercher Hochschule fuer Angewandte Wissenschaften (ZHAW)
- * School of Engineering (SoE)
- * InES Institut of Embedded Systems
+ * Copyright (c) 2018, Nordic Semiconductor
+ * All rights reserved.
  *
- *   Bachelorarbeit BA19_gruj_10
- *   Projekt FMS
- *   Darius Eckhardt (eckhadar)
- *   Ibrahim Evren   (evrenibr)
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
- *   07.06.2019, Winterthur Switzerland
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  *
- *   This Software is based on the ble_app_uart, SDK Version 15.2.0
- *   It has been modified to fit the needs of the Project FMS
- *   For correct functionality this Software has to be placed into the same folder as the SDK.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
  *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package ch.zhaw.android.measuringdata.utils;
